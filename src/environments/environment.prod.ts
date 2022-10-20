fb8185a98a55e863774da6f255d1534e03e88ff4
Maximiliano Vargas <Shilua4@gmail.com>
2022-10-20 00:55:08 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCKbN2xXPAZXidW_do5AlueRpxYQLiv6GM",
+    authDomain: "clinica-online-a0a2f.firebaseapp.com",
+    projectId: "clinica-online-a0a2f",
+    storageBucket: "clinica-online-a0a2f.appspot.com",
+    messagingSenderId: "196827085258",
+    appId: "1:196827085258:web:a1d1a63d42965658827733",
+    measurementId: "G-K2DCMWB9D6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCKbN2xXPAZXidW_do5AlueRpxYQLiv6GM",
+    authDomain: "clinica-online-a0a2f.firebaseapp.com",
+    projectId: "clinica-online-a0a2f",
+    storageBucket: "clinica-online-a0a2f.appspot.com",
+    messagingSenderId: "196827085258",
+    appId: "1:196827085258:web:a1d1a63d42965658827733",
+    measurementId: "G-K2DCMWB9D6"
+  }
 };
 
 /*
